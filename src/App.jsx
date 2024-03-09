f81dec3acf4495c441aed8ba15929b98198041ba
krishnakks143 <87386787+krishnakks143@users.noreply.github.com>
2024-03-09 19:45:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,8 +60,8 @@ function App() {
       <div className='flex items-center gap-x-1'>
         <input 
         type="range"
-        min={6}
-        max={100}
+        min={8}
+        max={20}
         value={length}
          className='cursor-pointer'
          onChange={(e) => {setLength(e.target.value)}}
