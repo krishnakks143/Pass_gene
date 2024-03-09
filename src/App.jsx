135a826868832bcbce531597f48398c28bd23c55
krishnakks143 <87386787+krishnakks143@users.noreply.github.com>
2024-03-09 19:37:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     <div  className="flex justify-center items-center h-screen">
-    <div className=" w-96 h-96 w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 ">
+    <div className=" w-120 h-96 w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-b0c4b1 text-orange-500 ">
       <h1 className='text-white text-center my-3'>Password generator</h1>
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
